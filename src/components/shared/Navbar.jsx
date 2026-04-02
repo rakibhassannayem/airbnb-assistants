390b4928e2a3bbb8314e97d0f26e8172a7180675
rakibhassannayem <rakibhassannayem@gmail.com>
2026-04-02 17:32:49 +0600

    navbar footer

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,76 +1,87 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { IoMdArrowForward } from "react-icons/io";
+import { usePathname } from "next/navigation";
+import Container from "./Container";
+
 const Navbar = () => {
-  return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+  const pathname = usePathname();
+
+  const navItems = [
+    { title: "Home", path: "/" },
+    { title: "About", path: "#" },
+    { title: "Services", path: "#" },
+    { title: "Pricing", path: "#" },
+    { title: "Blog", path: "#" },
+    { title: "Resources", path: "#" },
+  ];
+
+  const links = (
+    <>
+      {navItems.map((item, index) => {
+        const isActive = pathname === item.path;
+        return (
+          <li key={index}>
+            <Link
+              href={item.path}
+              className={`hover:text-primary transition-colors ${
+                isActive
+                  ? "text-primary font-bold underline underline-offset-10"
+                  : ""
+              }`}
             >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+              {item.title}
+            </Link>
+          </li>
+        );
+      })}
+    </>
+  );
+
+  return (
+    <div className="bg-base-100 shadow-sm py-1">
+      <Container className="navbar">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
           </div>
-          <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <Link href={"/"} className="">
+            <Image src="/google.png" alt="logo" height={25} width={125} />
+          </Link>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100 w-40 z-1">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
-      </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="flex px-1 gap-5 text-lg">{links}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn btn-primary text-white p-5 rounded-md">
+            Schecdule A Meeting <IoMdArrowForward size={17} />
+          </a>
+        </div>
+      </Container>
     </div>
   );
 };
